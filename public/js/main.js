904f2e255fb07936efdd9218bf228ea80f5ed038
Vlabson Viana <vlabsonsviana@gmail.com>
2022-09-13 16:58:02 -0300

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,19 @@
-var frase = $(".frase");
-console.log(frase);
+var frase = $(".frase").text();
+var numPalavras = frase.split(" ").length;
+var tamanhoFrase = $("#tamanho-frase");
+
+tamanhoFrase.text(numPalavras);
+
+
+
+var areaTxt = $(".campo-digitacao");
+areaTxt.on("input", function(){
+   
+    var txt = areaTxt.val();
+   
+    var qtdPalavras = txt.split(/\S+/).length -1;
+    $("#contador-palavras").text(qtdPalavras);
+   
+    var qtdCaracteres = txt.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+})
